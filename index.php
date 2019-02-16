ccf87c9980ff941d4c56a5dfaddd5fa6aec506b5
Nika-Hanover <kaleo@i.ua>
2019-02-16 15:28:28 +0200

    Add handling errors

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once('functions.php');
+
 function price_format($number){
     $number = ceil($number);
     $number = number_format($number, 0, ',', ' ');
@@ -14,31 +16,54 @@ $interval_hours = date('H:i', ($next_midnight - $current_date));
 $connect = mysqli_connect('localhost', 'root', '', 'yeticave');
 
 if($connect == false) {
-    print("Ошибка подключения: " . mysqli_connect_error());
+    $error = "Ошибка подключения: " . mysqli_connect_error();
+    $page_content = include_template('error.php',['error' => $error]);
+    $data = [
+        'content' => $page_content,
+        'title' => "Главная",
+        'is_auth' => $is_auth,
+        'user_name' => '',
+        'category' => []
+    ];
+    $html = include_template('layout.php', $data);
+    echo $html;
+    exit();
 }
-else {
-    $query_lots = "select l.lot_name name, c.categ_name category_name, l.start_price price, l.image img, l.step 
-            from lots l
-            join categories c on l.category_id = c.id
-            where date_format(date_close,'%Y-%m-%d') > date_format(SYSDATE(),'%Y-%m-%d')
-            order by date_creation desc";
-    $query_categories = "select categ_name name from categories";
 
-    $res_lots = mysqli_query($connect, $query_lots);
-    $res_categories = mysqli_query($connect, $query_categories);
+$query_lots = "select l.lot_name name, c.categ_name category_name, l.start_price price, l.image img, l.step 
+        from lots l
+        join categories c on l.category_id = c.id
+        where date_format(date_close,'%Y-%m-%d') > date_format(SYSDATE(),'%Y-%m-%d')
+        order by date_creation desc";
+$query_categories = "select categ_name name from categories";
 
-    if (!$res_lots || !$res_categories) {
-        $error = mysqli_error($connect);
-        print("Ошибка MySQL: ".$error);
-    } 
+$res_lots = mysqli_query($connect, $query_lots);
+$res_categories = mysqli_query($connect, $query_categories);
+$cat_num = mysqli_num_rows($res_categories);
+$lot_num = mysqli_num_rows($res_lots);
 
-    $category = mysqli_fetch_all($res_categories, MYSQLI_ASSOC);
-    $lots_list = mysqli_fetch_all($res_lots, MYSQLI_ASSOC);
-}
+if (!$res_lots || !$res_categories || $cat_num == 0) {
+    if ($cat_num == 0) $error = 'Categories quantity is 0.';
+    else $error = mysqli_error($connect);
+    $page_content = include_template('error.php',['error' => $error]);
+    $data = [
+        'content' => $page_content,
+        'title' => "Главная",
+        'is_auth' => $is_auth,
+        'user_name' => '',
+        'category' => []
+    ];
+    $html = include_template('layout.php', $data);
+    echo $html;
+    exit();
+} 
+
+$category = mysqli_fetch_all($res_categories, MYSQLI_ASSOC);
+$lots_list = mysqli_fetch_all($res_lots, MYSQLI_ASSOC);
+
 
 $user_name = 'Nika'; // укажите здесь ваше имя
 
-require_once('functions.php');
 $page_content = include_template('index.php',['category' => $category,
                                             'lots_list' => $lots_list,
                                             'interval_hours' => $interval_hours
---
+++ templates/error.php
@@ -0,0 +1,10 @@
+<main class="content">
+	<div class="content_main_col">
+		<div class="lots__header">
+			<h2>Ошибка</h2>
+		</div>
+		<article class="lots__list">
+			<p><?= $error ? $error : "Похоже что-то пошло не так :("?></p>
+		</article>
+	</div>
+</main>
